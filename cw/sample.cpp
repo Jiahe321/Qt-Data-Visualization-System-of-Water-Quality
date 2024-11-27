142aca4d81e71a30fffdbd7fcc37817c48bdeff5
Rastry <146269555+Jiahe321@users.noreply.github.com>
2024-11-27 15:11:11 +0000

    Delete cw directory

--- cw/sample.cpp
+++
@@ -1,8 +0,0 @@
-#include "sample.hpp"
-
-std::ostream& operator<<(std::ostream& os, const WaterSample& sample) {
-    return << "ID: " << sample.getId()
-        << "\nSampling Point: " << sample.getSamplingPoint()
-        << "\nSampling DateTime: " << sample.getSamplingDateTime()
-        << 
-}
